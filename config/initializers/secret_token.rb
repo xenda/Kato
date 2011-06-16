54f1e9025703cf1f3fb440a8411b5cd456bad1de
Alvaro Pereyra <alvaro@xendacentral.com>
2011-06-16 13:18:18 -0500

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kato::Application.config.secret_token = '74d62847d6d0f9c075b4ee85c9b852ea861093eb9de74c77dac9ed53afccb0a85bb8927e54c6619fb596111603ae68092e093c9e77b300db87f23fc65619e3ea'
