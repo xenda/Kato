0015245b0859d8be28d88973159b1ecc55308037
Alvaro Pereyra <alvaro@xendacentral.com>
2011-06-21 00:31:07 -0500

    Fixes how the images and videos are loaded

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -8,17 +8,45 @@ function sizeChangeCallback() {
 }
 
 
-
-
 $(function(){
 
   var $container = $('#messages_list');
   $container.imagesLoaded(function(){
     $container.masonry({
-      itemSelector : '.message'
+      itemSelector : '.message',
+      isAnimated : true
     });
   });
 
 
+  $(".message").each(function(value){ 
+
+  // Capture each comment text
+  text = $(this).html();
+
+    	// We match the URL pattern
+    	regMatches = (text.match(/\b((?:[a-z][\w-]+:(?:\/{1,3}|[a-z0-9%])|www\d{0,3}[.])(?:[^\s()<>]+|\([^\s()<>]+\))+(?:\([^\s()<>]+\)|[^`!()\[\]{};:'".,<>?«»“”‘’\s]))/gi));
+        if (regMatches) {
+    	// We iterate from each matches
+    	$.each(regMatches, function(index,value){
+        	reg_value = value.replace("?","\\?")
+        	var re = new RegExp("\\s*"+reg_value+"\\s*","gim");
+        	//console.debug(re);
+        	randId = Math.floor(Math.random()*1000);
+        	mediaId = "oembed_media_" + randId;
+        	text = text.replace(re,"<a href='"+ value +"' class='oembed hidden media' id='"+mediaId+"'>"+value+"</a>");
+    	})
+    	$(this).html(text);
+  	}
+  });
+
+  $(".oembed").oembed(null,
+    {
+      embedMethod:"append", maxWidth:"220",
+      afterEmbed: function(oembedData) {
+        $container.imagesLoaded(function(){
+          $container.masonry( 'reload' );})
+    }
+    });
 });
 
