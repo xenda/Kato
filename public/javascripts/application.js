ff5144eb455500afb99602d0f379ce299e0f260d
Alvaro Pereyra <alvaro@xendacentral.com>
2011-06-20 19:02:48 -0500

    Adds masonry and better update of votes

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,7 +1,24 @@
-window.fbAsyncInit = function() {
-FB.Canvas.setSize();
-}
+  window.fbAsyncInit = function() {
+  FB.Canvas.setSize();
+  }
 // Do things that will sometimes call sizeChangeCallback()
 function sizeChangeCallback() {
-FB.Canvas.setSize();
+  FB.Canvas.setSize();
+
 }
+
+
+
+
+$(function(){
+
+  var $container = $('#messages_list');
+  $container.imagesLoaded(function(){
+    $container.masonry({
+      itemSelector : '.message'
+    });
+  });
+
+
+});
+
