3599268e5e9023c631ffbe05957a15921a44bf35
Alvaro Pereyra <alvaro@xendacentral.com>
2011-06-21 15:20:21 -0500

    Updates with more content

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -78,20 +78,9 @@ $(function(){
 function addNewMessage(data){
   
   var token = $('meta[name="csrf-token"]').attr('content')
-  
-  $("#messages_list").prepend('<li id=\"message_' + data['id'] +'\" class=\"message masonry-brick\" style=\"position: absolute;
-top: 10px; left: 10px;\"> <h4>' + data['title'] +'</h4> <div class=\"content\">'+ data['content']
-+'</div> <div class=\"meta_data\"> <div class=\"avatar\"></div> <div class=\"name\"></div> <div
-class=\"votes\"> <div class=\"vote_count\">' + data['votes_count'] + ' votos </div> <div
-class=\"submit\"> <form method=\"post\" id=\"new_vote\" data-remote=\"true\" class=\"simple_form
-vote\" action=\"/votes\" accept-charset=\"UTF-8\"><div style=\"&lt;a
-href=\'margin:0;padding:0;display:inline\' class=\'oembed hidden media\'
-id=\'oembed_media_503\'&gt;margin:0;padding:0;display:inline&lt;/a&gt;\"><input type=\"hidden\"
-value=\"✓\" name=\"utf8\"><input type=\"hidden\" value=\"' + token +'\"
-name=\"authenticity_token\"></div> <input type=\"hidden\" value=\"' + data['id'] +'\"
-name=\"vote[message_id]\" id=\"vote_message_id\" class=\"hidden\"> <input type=\"submit\"
-value=\"Votar\" name=\"commit\" id=\"vote_submit\" class=\"button\"> </form> </div> </div> </div>
-</li>');
+  // var data = [];
+  // data['id']  = "lala";
+  $("#messages_list").prepend('<li class="message" id="message_'+ data["id"] +'"><h4><a href="/messages/'+data["id"]+'">'+ data["title"]+'</a></h4><div class="content">'+ data["content"] +'</div><div class="meta_data"> <div class="avatar"></div> <div class="name"></div> <div class="votes"><div class="vote_count"> '+ data["votes_count"]+' votos</div> <div class="submit"> <form accept-charset="UTF-8" action="/votes" class="simple_form vote" data-remote="true"   id="new_vote" method="post"><div style="margin:0;padding:0;display:inline"><input name="utf8" type="hidden" value="✓"><input name="authenticity_token" type="hidden" value="'+ token +'"></div> <input class="hidden" id="vote_message_id" name="vote[message_id]" type="hidden" value="'+ data["id"]+'"> <input class="button" id="vote_submit" name="commit" type="submit" value="Votar"> </form> </div> </div> </div> </li>');
 }
 
 function updateVotesCount(data){
