fbd49ae71195760878a668fefdf290fa845d0ab1
HPNeo <hpneo@hotmail.com>
2011-08-01 14:27:44 -0700

    tooltips

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -21,15 +21,15 @@ $(function(){
 // }
 
 $('li.message').each(function(index,item){
-  $(item).append('<div class="tooltip" id="tooltip-' + $(item).attr('title').toLowerCase().replace(" ", "_") + '"><div class="content">' + $(item).attr('title') + '</div><div class="arrow"></div></div>');
+  $(item).append('<div class="tooltip" id="tooltip_' + $(item).attr('id') + '"><div class="content">' + $(item).attr('title') + '</div><div class="arrow"></div></div>');
 });
 
 $('li.message').mouseover(function(){
-  var id = '#tooltip-' + $(this).attr('title').toLowerCase().replace(" ", "_");
+  var id = '#tooltip_' + $(this).attr('id');
   $(id).fadeIn('fast');
 });
 $('li.message').mouseout(function(){
-  var id = '#tooltip-' + $(this).attr('title').toLowerCase().replace(" ", "_");
+  var id = '#tooltip_' + $(this).attr('id');
   $(id).fadeOut('fast');
 });
 
