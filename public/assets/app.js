228be639eb4d4ba4468f424816f2bdb936a3d234
HPNeo <hpneo@hotmail.com>
2011-08-01 15:12:51 -0700

    tooltips

--- public/assets/app.js
+++ public/assets/app.js
@@ -564,15 +564,12 @@ $('li.message').each(function(index,item){
   $(item).append('<div class="tooltip" id="tooltip_' + $(item).attr('id') + '"><div class="content">' + $(item).attr('title') + '</div><div class="arrow"></div></div>');
 });
 
-$('li.message').mouseenter(function(){
-  var id = '#tooltip_' + $(this).attr('id');
-    $(id).css({opacity:0.7, display:"none"}).fadeIn('fast');
-}).mousemove(function(){
-  var id = '#tooltip_' + $(this).attr('id');
-    $(id).show();
+$('li.message .tooltip').mouseenter(function(){
+  //var id = '#tooltip_' + $(this).attr('id');
+    $(this).fadeIn('fast');
 }).mouseleave(function(){
-  var id = '#tooltip_' + $(this).attr('id');
-    $(id).fadeOut('fast');
+  //var id = '#tooltip_' + $(this).attr('id');
+    $(this).fadeOut('fast');
 });
 
 //$("li.message").tipsy({gravity: 's', fade:true});
