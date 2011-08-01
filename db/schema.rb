76af8471ee20606d37635f952128e01e77071879
Alvaro Pereyra <alvaro@xendacentral.com>
2011-08-01 15:56:24 -0500

    Adds migration and updates styling

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110730173215) do
+ActiveRecord::Schema.define(:version => 20110801190650) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.integer  "resource_id",   :null => false
@@ -57,9 +57,9 @@ ActiveRecord::Schema.define(:version => 20110730173215) do
 
   create_table "messages", :force => true do |t|
     t.integer  "user_id"
-    t.string   "title",              :default => "Mi idea", :null => false
+    t.string   "title",              :default => "Mi receta...", :null => false
     t.text     "content"
-    t.integer  "votes_count",        :default => 0,         :null => false
+    t.integer  "votes_count",        :default => 0,              :null => false
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "photo_file_name"
