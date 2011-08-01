13740c31c1f2605ecfd1d9ddecc8059ed273a8cf
Alvaro Pereyra <alvaro@xendacentral.com>
2011-08-01 00:35:53 -0500

    Updates with S3 support

--- app/models/message.rb
+++ app/models/message.rb
@@ -6,7 +6,13 @@ class Message < ActiveRecord::Base
 
   validates_presence_of :title, :on => :create, :message => "no puede estar vac&iacute;o"
 
-  has_attached_file :photo, :styles => {:thumb => "180x150#", :big => "570x300#"}
+  has_attached_file :photo,
+    :styles => {:thumb => "180x150#", :big => "570x300#"},
+    :storage => :s3,
+    :s3_credentials => "#{RAILS_ROOT}/config/s3.yml",
+    :path => '/:attachment/:id/:filename'
+
+
 
   scope :most_voted, limit(4).order("votes_count DESC")
 
