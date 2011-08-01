7ae72c22157bba3b3c308a01fc39338870818983
HPNeo <hpneo@hotmail.com>
2011-08-01 09:40:23 -0700

    CSS para que se vea bonito en IE 7-9

--- public/assets/app.js
+++ public/assets/app.js
@@ -520,6 +520,12 @@ $(function(){
   //   });
   // });
 
+if (window.PIE) {
+  $('#messages_list li, ul#most_voted li, div.message').each(function(){
+    PIE.attach(this);
+  });
+}
+
 window.fbAsyncInit = function() {
   FB.Canvas.setSize();
 }
