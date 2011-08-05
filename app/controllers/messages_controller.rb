d998672202bbb7eacbccbf20be7c62eb84d3dc17
Alvaro Pereyra <alvaro@xendacentral.com>
2011-08-05 01:58:13 -0500

    Moves to signed request

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,6 +3,51 @@ class MessagesController < InheritedResources::Base
   #before_filter :relog_user!
   #after_filter :send_to_pusher, :only => :create
   before_filter :load_most_voted
+  before_filter :setup_code
+
+  def setup_code
+
+
+     if params[:signed_request]
+      fb_auth_client.from_signed_request(params[:signed_request])
+      if fb_auth_client.authorized?
+        # If authorized, the auth has user and access_token.
+        # redirect_to user_fb_auth_path unless current_user
+        unless current_user
+          @data = fb_auth_client.data
+          logger.info @data["user"].inspect
+          user = @data["user_id"]
+          token = @data["oauth_token"]
+
+          resource = User.find_by_facebook_uid(user)
+          if User.respond_to?(:serialize_into_cookie)
+            resource.remember_me!
+            cookies.signed["remember_user_token"] = {
+              :value => User.serialize_into_cookie(resource),
+              :expires => resource.remember_expires_at,
+              :path => "/"
+            }
+          # logger.info cookies.signed["remember_#{resource_name}_token"].inspect
+          end
+          sign_in("users", resource)
+          # set_flash_message :notice, :signed_in
+        end
+
+        @data = fb_auth_client.data
+        logger.info @data["user"].inspect
+        user = @data["user_id"]
+        token = @data["oauth_token"]
+        cookies.permanent["fbtoken"] = token
+
+      else
+        # First time user, show "Connect" button here.
+        redirect_to user_fb_auth_path
+        # @data = auth.data
+      end
+    end
+
+  end
+
 
   def create
     create! {
@@ -59,7 +104,7 @@ class MessagesController < InheritedResources::Base
     unless current_user
         if cookies.permanent["fbtoken"]
           resource = User.find_by_facebook_token(cookies.permanent["fbtoken"])
-          if resource          
+          if resource
             if User.respond_to?(:serialize_into_cookie)
               resource.remember_me!
               cookies.signed["remember_user_token"] = {
