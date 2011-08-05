13279f99cfed73ca4ff2ea373df0e2a66b327870
HPNeo <hpneo@hotmail.com>
2011-08-04 21:04:17 -0700

    Pequeños cambios

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -17,7 +17,7 @@ $(function(){
 
   window.setTimeout(function() {
     $('.fb_dialog .fb_dialog_advanced .loading').css({'top':'82.5px', 'left':'116.5px'});
-  }, 250);
+  }, 5);
 
   $('#contest_logo a').click(function(event){
     event.preventDefault();
@@ -157,9 +157,6 @@ function streamPublish(name, caption, link, description, picture){
             description: description,
             display : 'iframe',
             access_token: fb_token
-          }, function(response){
-            $('.fb_dialog .fb_dialog_advanced .loading').css({'top':'82.5px', 'left':'116.5px'});
-            alert($('.fb_dialog .fb_dialog_advanced .loading').html());
           });
         //http://developers.facebook.com/docs/reference/dialogs/feed/
 }
