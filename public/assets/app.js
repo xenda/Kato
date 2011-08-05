a8c036b7ca7c8e4f2afaafed48de9580a9fa1b1d
HPNeo <hpneo@hotmail.com>
2011-08-04 20:31:50 -0700

    Pequeños cambios

--- public/assets/app.js
+++ public/assets/app.js
@@ -693,8 +693,8 @@ function streamPublish(name, caption, link, description, picture){
             display : 'iframe',
             access_token: fb_token
           }, function(response){
-            $('.fb_dialog .fb_dialog_advanced .loading').css({'top':'82.5px', 'left':'116.5px'});
-            console.log($('.fb_dialog'));
+            if(response)
+              $('.fb_dialog .fb_dialog_advanced .loading').css({'top':'82.5px', 'left':'116.5px'});
           });
         //http://developers.facebook.com/docs/reference/dialogs/feed/
 }
