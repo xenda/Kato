4df1756bf14115fccbc40534702268091d631444
Alvaro Pereyra <alvaro@xendacentral.com>
2011-08-17 09:30:44 -0500

    Updates deploy

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,18 @@
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
 require 'csv'
-
-CSV.foreach("bd2.csv", :headers => true) do |row|
+x = 0
+CSV.foreach("mundosueldo.csv", :headers => true) do |row|
       fields = *row.fields
       fields = fields.map &:strip
       document_type, document_number, middle_name, last_name, name = fields
       # puts document_number
       unless Client.find_by_document_number(document_number) 
       	Client.create(:document_number => document_number, :document_type => document_type, :middle_name => middle_name, :last_name => last_name, :name => name)
+      	x +=1
+      	puts "#{x} - #{document_number}"
+      	logger.info "#{x} - #{document_number}"
+      	say "#{x} - #{document_number}"
       end
       # Client.connection.execute("insert into clients (document_number, document_type, middle_name, last_name, name) values ('#{document_number}','#{document_type}','#{middle_name}','#{last_name}','#{name}')")
 end
