f147dc28468901cd903eb11763a62b325fd3264e
HPNeo <hpneo@hotmail.com>
2011-08-22 14:45:37 -0500

    Quitar campos del admin

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,10 +1,10 @@
 ActiveAdmin.register User do
 
   index do
-    column :id do |user|
+    column :id
+    column :email do |user|
       auto_link(user)
     end
-    column :email
     column :name
     column :facebook_token
     column :facebook_uid
